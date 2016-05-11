bf8c189d5d66b9e6f79ff42f066cdc4007b7cc2a
godfatherfrancis <francis.aarons@gmail.com>
2016-05-11 13:24:41 -0400

    First Android Chat Client commit

---
+++ app/src/main/java/mobile/tiy/chatclientandroid/MainActivity.java
@@ -0,0 +1,18 @@
+package mobile.tiy.chatclientandroid;
+
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+}
